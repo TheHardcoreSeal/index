f25be241778a06ee92d250d03d8e7a8f6deb9072
Dylan <dylan.todd04@gmail.com>
2023-11-23 23:14:21 -0500

    added functionality to discovery hub

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+var exploreVisible = false;
+
+$( function() {
+    $( "#sortable" ).sortable({
+        handle: 'i',
+        items: '> div:not(#nonSortable)',
+        axis: 'y',
+        containment: 'parent',
+        helper: function(e, ui) {
+            ui.children().each(function() {
+                $(this).width($(this).width());
+            });
+            return ui.clone().addClass('drag-panels-helper');
+        }
+    });
+    $( "#sortable" ).disableSelection();
+} );
+
+function xPanelClick(){
+    document.getElementById('overlay').style.visibility = 'hidden';
+}
+
+function editPanelsClick(){
+    document.getElementById('overlay').style.visibility = 'visible';
+}
+
 function shortcutsClick() {
     var newHtml = '<p style="margin:0; font-size: larger; font-weight:300; color: #b2b2b2;">You have no shortcuts.</p>';
     var container = document.getElementById('shortcutClickDiv');
-    var shortcutArrow = document.getElementById('shortcutArrow')
+    var shortcutArrow = document.getElementById('shortcutArrow');
 
     if (container.innerHTML === newHtml) 
     {
@@ -20,24 +46,49 @@ function shortcutsClick() {
     }
 }
 
-function exploreClick()
-{
+function exploreClick() {
     var container = document.getElementById('exploreClickDiv');
-    var exploreArrow = document.getElementById('exploreArrow')
+    var exploreArrow = document.getElementById('exploreArrow');
 
-    if (container.style.visibility == 'hidden') 
-    {
-        container.style.visibility = 'visible'
+    if (container.style.visibility === 'hidden') {
+        container.style.visibility = 'visible';
         exploreArrow.style.transition = 'transform 0.3s ease, color 0.3s ease';
         exploreArrow.style.transform = 'rotate(90deg)';
         exploreArrow.style.color = '#0866FF';
-    } 
 
-    else 
-    {
-        container.style.visibility = 'hidden'
+        exploreVisible = true;
+    } else {
+        container.style.visibility = 'hidden';
         exploreArrow.style.transition = 'transform 0.3s ease, color 0.3s ease';
         exploreArrow.style.transform = 'rotate(180deg)';
         exploreArrow.style.color = '#FFFFFF';
+
+        exploreVisible = false;
     }
+}
+
+function discoveryHubClick()
+{
+    var container1 = document.getElementById('side1');
+    var container2 = document.getElementById('side2');
+
+    container1.style.visibility = 'hidden';
+    document.getElementById('exploreClickDiv').style.visibility = 'hidden'
+
+    container2.style.visibility = 'visible';
+}
+
+function linksClick()
+{
+    var container1 = document.getElementById('side1');
+    var container2 = document.getElementById('side2');
+
+    container1.style.visibility = 'visible';
+    if (exploreVisible)
+    {
+        document.getElementById('exploreClickDiv').style.visibility = 'visible'
+    }
+    
+
+    container2.style.visibility = 'hidden';
 }
